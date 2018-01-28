acc3e43c7c3fb8f72b25bb140c5c24e5e0b3fc3e
Jenna Johnson <MillaJAB@users.noreply.github.com>
2018-01-27 22:23:50 -0600

    Removed friction/decay from code

--- js/Player.js
+++ js/Player.js
@@ -1,4 +1,3 @@
-const GROUNDSPEED_DECAY_MULT = 0.94;
 const DRIVE_POWER = 0.5;
 const REVERSE_POWER = 0.2;
 const TURN_RATE = 0.06;
@@ -52,8 +51,6 @@ function carClass() {
 	} // end of carReset func
 
 	this.move = function() {
-		this.speed *= GROUNDSPEED_DECAY_MULT;
-
 		if (this.keyHeld_Gas) {
 			this.speed += DRIVE_POWER;
 		}
